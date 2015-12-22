789f017a666690302291349c6a43f99c0f407f5f
Sasha <moon_lady_@ukr.net>
2015-12-22 11:34:05 +0200

    Initial commit

---
+++ Assets/_Scene/Scripts/backpack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class backpack : MonoBehaviour {
+public Text showText;
+public Text count;
+public GameObject gameObject;
+private int countInt;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		setText("Press E");
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (Input.GetKeyUp(KeyCode.E)) {
+			Destroy(gameObject);
+			setText("");
+			countInt = System.Int32.Parse(count.text);
+			countInt++;
+			count.text = countInt.ToString(); 
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		setText("");
+	}
+	
+	void setText(string a)
+	{
+		showText.text = a;
+	}
+}
---
+++ Assets/_Scene/Scripts/freezer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class freezer : MonoBehaviour {
+public Text showText;
+public Text count;
+public GameObject gameObject;
+private int countInt;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		setText("Press E");
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (Input.GetKeyUp(KeyCode.E)) {
+			Destroy(gameObject);
+			setText("");
+			countInt = System.Int32.Parse(count.text);
+			countInt++;
+			if (countInt >= 4) {
+				count.text = "All!";
+			} else {
+				count.text = countInt.ToString(); 
+			}
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		setText("");
+	}
+	
+	void setText(string a)
+	{
+		showText.text = a;
+	}
+}
